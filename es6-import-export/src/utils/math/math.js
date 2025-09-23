67099fcacb0f5e0cfe111758f91894086af361a9
Aion <aionshahriar23@gmail.com>
2025-09-23 23:56:36 +0600

    es6 import export m-1 added

---
+++ es6-import-export/src/utils/math/math.js
@@ -0,0 +1,14 @@
+
+
+const add = (a,b) => a+b;
+
+const dif = (a,b) => a-b;
+
+
+const multiply = (a,b) => a*b;
+
+
+const divide = (a,b) => a/b;
+
+export {add, dif, multiply as mul, divide}
+
---
+++ es6-import-export/src/utils/tax/tax.js
@@ -0,0 +1,5 @@
+import { mul } from "../math/math";
+
+
+const calTax=mul(2000*0.15);
+console.log(calTax);
